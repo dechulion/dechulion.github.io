f0e47a1ba16799017773dc87cf6d55fe568fec18
dechulion <dechulion@yahoo.com>
2013-11-07 13:59:21 -0800

    Restructure Portfolio to work with silex and twig

---
+++ src/app.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\SwiftmailerServiceProvider());
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/views'
+));
+
+$app->before(function () use ($app) {
+  $app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.twig'));
+});
+
+$app->get('/', function () use ($app) {
+  return $app['twig']->render('index.html.twig');
+});
+
+$app->get('/defending', function () use ($app) {
+  return $app['twig']->render('defending.html.twig');
+});
+
+$app->get('/css', function () use ($app) {
+  return $app['twig']->render('css.html.twig');
+});
+
+$app->get('/php', function () use ($app) {
+  return $app['twig']->render('php.html.twig');
+});
+
+return $app;
---
+++ web/js/custom.js
@@ -0,0 +1,45 @@
+/* Add here all your JS customizations */
+
+$(".modalities_carousel").carouFredSel({
+    width: 809,
+    height: 250,
+    items: {
+        visible: 1,
+        minimum: 2,
+        width: 800,
+        height: 240
+    },
+    scroll: {
+        fx: "fade",
+        pauseOnHover: true,
+        duration: 1000
+    },
+    auto: 6000,
+});
+
+
+
+$(".testimonial_carousel").carouFredSel({
+    width: 240,
+    height: 230,
+    items: {
+        visible: 1,
+        start: 1,
+        width: 210,
+        height: 220
+    },
+    scroll: {
+        easing: "linear",
+        duration: 1000,
+        pauseOnHover: true
+    },
+    auto: 5000
+});
+
+$(document).ready(function() {
+    $(".menu [href]").each(function() {
+    if (this.href == window.location.href) {
+        $(this).addClass("active");
+        }
+    });
+});
